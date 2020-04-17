26918a6ea1870e7325b0672d59f43b75af61cb4d
TheBlackKoala <torskovsgaard@hotmail.com>
2020-04-17 15:22:55 +0200

    Comment added

--- indicators/notification.js
+++ indicators/notification.js
@@ -123,6 +123,7 @@ var MessagesIndicator = new Lang.Class({
         });
     },
     _onSourceAdded: function (tray, source) {
+        //IS THIS NEEDED?
         //source.connect('count-updated', () => this._updateCount());
         //this._sources.push(source);
         this._updateCount();
